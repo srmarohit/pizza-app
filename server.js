f4b79fc3b5425947d5c81c319d94529494ea0f08
Rohit Sharma <srmarohit@gmail.com>
2021-04-29 10:02:58 +0530

    have created index cart login registered page

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300 ;
 
-// use Routes befor use and set method.
-app.get('/', (req, res) => {
-    res.render('index')
-})
+//use public assets
+app.use(express.static('public'))
 
 // set Template engine after routes
 
@@ -16,6 +14,26 @@ app.use(expressLayout);
 app.set('views', path.join(__dirname,'./resources/views'));
 app.set('view engine', 'ejs');
 
+
+// use Routes after use and set method.
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// cart get method 
+app.get('/cart', (req,res)=>{
+    // Dont use / in front of render path.
+    res.render('customers/cart');
+})
+
+// auth get method
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
 
 
 
